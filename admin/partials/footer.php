9f78828798c29515b060a2da94ff8ecefdc8d843
ronaldssekagiri <113431544+ronaldssekagiri@users.noreply.github.com>
2026-09-22 21:28:49 +0300

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2026 All rights reserved@Habicakes</p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
